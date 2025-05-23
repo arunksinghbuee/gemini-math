6ef08f0de5645f2048fef1374d09032c55c7baf3
Arun Kumar Singh <133665028+arunksinghbuee@users.noreply.github.com>
2025-05-23 15:59:22 +0530

    class-11-misc-ch-2

--- class-11/math/ncert/questions/main-misc-questions.py
+++ class-11/math/ncert/questions/main-misc-questions.py
@@ -15,14 +15,14 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 CLASS_NAME = "class 11"
-CHAPTER_NUMBER = "1"
+CHAPTER_NUMBER = "2"
 #EXERCISE_NUMBER = "2.2"
-EXERCISE_CODE = "MISCELLANEOUS-EXERCISE-CHAPTER-1"
+EXERCISE_CODE = "NCERT-MISCELLANEOUS-EXERCISE-CHAPTER-2"
 
 STATUS = "PUBLISHED"
 GRADE_CODE = "GRADE-11"
 SUBJECT_CODE = "MATH"
-TOPIC_CODE = "SETS"
+TOPIC_CODE = "RELATIONS-AND-FUNCTIONS"
 POSTED_BY_USER_ID = "6810b82fb49f7e3b1f0460ea"
 BOARD = "CBSE"
 SOURCE = "NCERT Maths"
@@ -34,7 +34,7 @@ def call_process_pdf_api(attempt):
         
         # Form data
         files = {
-            'pdf_file': ('class-11-misc-ch-1.pdf', open('../book/ch-1/misc-ch-1.pdf', 'rb'), 'application/pdf')
+            'pdf_file': ('class-11-misc-ch-2.pdf', open('../book/ch-2/misc-ch-2.pdf', 'rb'), 'application/pdf')
         }
         
         data = {
